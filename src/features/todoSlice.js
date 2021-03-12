ba9d6d59361254db2e724c67823ba4ce0d552600
Carmen Odimba <carmen.odimba@laposte.net>
2021-03-12 23:50:49 +0100

    functionnalities

--- src/features/todoSlice.js
+++ src/features/todoSlice.js
@@ -15,10 +15,25 @@ const todoSlice = createSlice({
             //state = current state of the saveTodo, action = function that gets executed when
             //the redux is shot in
             state.todoList.push(action.payload)
+        },
+
+        setCheck: (state, action) => {
+            // map through to do list
+            state.todoList.map(item=>{
+                if (action.payload === item.id) {
+                    if (item.done === true) {
+                        item.done = false
+                    } else {
+                        item.done=true
+                    }
+                }
+            })
         }
     }
 });
 
-export const { saveTodo } = todoSlice.actions
+export const { saveTodo, setCheck } = todoSlice.actions
+
+export const selectTodoList = state => state.todos.todoList
 
 export default todoSlice.reducer
